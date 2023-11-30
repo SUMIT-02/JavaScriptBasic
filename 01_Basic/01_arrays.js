cb60d384ce5e5ec5d6060ffde2df0f60a0b07dd0
SUMIT-02 <85380191+SUMIT-02@users.noreply.github.com>
2023-11-30 06:54:30 +0000

    ArraysBasic

---
+++ 01_Basic/01_arrays.js
@@ -0,0 +1,26 @@
+const myArr = [0,1 , 2 ,3 , 4, 5]
+const myHeors =["xyz","abc"]
+//console.log(myArr[1]);
+
+const myArr2=new Array(1,2,3,4)
+//console.log(myArr[1]);
+
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.push(6)
+// myArr.pop()
+
+myArr.unshift(9)
+myArr.unshift(9)
+console.log(myArr);
+
+
+
+//slice ,splice
+
+
+// console.log("A",myArr);
+
+// const myn1 = myArr.slice(1,3)
