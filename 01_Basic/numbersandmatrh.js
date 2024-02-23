9a192fc4c236f174f4b73b66c82e422c35d0977b
SUMIT-02 <85380191+SUMIT-02@users.noreply.github.com>
2024-02-23 05:06:03 +0000

    methods

---
+++ 01_Basic/numbersandmatrh.js
@@ -0,0 +1,31 @@
+const score=400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//.log(balance.toFixed(1));
+
+
+const otherNumber = 123.6789
+
+//console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+///+++++++++++++maths++++++++++
+
+console.log(Math);
+
+const abc = -4
+
+console.log(Math.abs(abc))
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.min(4,5,6,7,8,9));
+console.log(Math.max(4,6,8,59,4));
