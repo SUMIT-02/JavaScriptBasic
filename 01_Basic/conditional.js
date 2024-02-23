f6086c867b97f1e012e1c51a3a34e0662c7a1d6b
SUMIT-02 <85380191+SUMIT-02@users.noreply.github.com>
2024-02-23 05:00:59 +0000

    logical gates

---
+++ 01_Basic/conditional.js
@@ -0,0 +1,13 @@
+const age = 18;
+if(age>=18){
+    console.log('yes can vote');
+}
+else if (age>=80){
+    console.log('Yes youcan vote');
+}
+
+let result = age>=18 ? 'yes' : 'no';
+
+console.log(result);
+
+
---
+++ 01_Basic/logical.js
@@ -0,0 +1,25 @@
+
+// And = all condition must be true 
+//or = check any startment is true 
+
+
+
+const age = 2;
+const gender = 'male';
+
+if(age>=18 || gender == 'female'){
+    console.log('you Are Adult Male');
+}
+else{
+    console.log('not');
+}
+
+
+
+
+const number = 5 ;
+
+if(number%2 != 0){
+    console.log('odd');
+}
+
